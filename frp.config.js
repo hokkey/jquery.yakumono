a56a9c9921f28436bb264e2462017f526c645b76
Yuma Hori <hokkey@mac.com>
2017-02-11 19:57:51 +0900

    chore: ビルド構成の変更
    blast.jsをリポジトリから除外し、デモHTMLからはCDNを参照するように変更。demoディレクトリの内容をdistディレクトリに統合

--- frp.config.js
+++ frp.config.js
@@ -2,19 +2,18 @@
 // https://github.com/frontainer/frontplate-cli/wiki/6.%E8%A8%AD%E5%AE%9A
 module.exports = function(production) {
   global.FRP_SRC = 'src';
-  global.FRP_DEST = 'public';
+  global.FRP_DEST = 'dev';
   return {
     clean: {},
     html: {},
-    style: production ? {} : {},
-    script: production ? {} : { },
+    style: production ? {
+        dest: FRP_DEST
+      } : {
+        dest: FRP_DEST
+      },
+    script: production ? {} : {},
     server: {},
-    copy: {
-      // blast.js
-      './node_modules/blast-text/jquery.blast.min.js': './public/assets/js',
-      // normalize.css
-      './node_modules/normalize.css/normalize.css': './public/assets/css'
-    },
+    copy: {},
     sprite: [],
     test: {}
   }
