6ae750a681fc2b9d79005bf605c7e0ec25d691b4
Yuma Hori <hokkey@mac.com>
2017-02-11 19:43:15 +0900

    feat: バージョン0.3.0
    - 行末約物半角の追加。
    - 行頭・行末の判定後に字間調整が必要な場合の処理を追加。
    - Safari用ポリフィルの追加
    - CSS名前付けルールを変更し
    - BEMのセパレータを設定で変更できるようにした

--- dist/jquery.yakumono.js
+++ dist/jquery.yakumono.js
@@ -1,3 +1,7 @@
+"use strict";
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
 /**!
   @license
   jQuery.yakumono (0.3.0) github.com/hokkey/jquery.yakumono
@@ -5,18 +9,17 @@
   MIT license: en.wikipedia.org/wiki/MIT_License
 */
 (function (factory) {
-  if (typeof module === "object" && typeof module.exports === "object") {
+  if ((typeof module === "undefined" ? "undefined" : _typeof(module)) === "object" && _typeof(module.exports) === "object") {
     module.exports = factory(require("jquery"), window, document);
-  }
-  else {
+  } else {
     factory(jQuery, window, document);
   }
-}(function ($, window, document, undefined) {
+})(function ($, window, document, undefined) {
 
-  const methods = {
+  var methods = {
 
     // yakumono
-    yakumono: ($elems, options) => {
+    yakumono: function yakumono($elems, options) {
       if ($elems.length === 0) return $elems;
 
       if ($.type($().blast) !== 'function') {
@@ -24,7 +27,7 @@
         return $elems;
       }
 
-      const settings = $.extend({}, $.fn.yakumono.defaults, options);
+      var settings = $.extend({}, $.fn.yakumono.defaults, options);
       settings.safariMode = methods.checkSafari(settings);
 
       $elems.each(function () {
@@ -37,7 +40,7 @@
     },
 
     // Safariであるかを判定
-    checkSafari: (settings) => {
+    checkSafari: function checkSafari(settings) {
       if (settings.polyfillSafari !== true) {
         return false;
       }
@@ -45,7 +48,7 @@
     },
 
     // 初期化処理
-    init: ($elem, settings) => {
+    init: function init($elem, settings) {
       if (settings.safariMode) {
         $elem.addClass(settings.className.safari);
       }
@@ -62,7 +65,7 @@
       methods.blast($elem, settings);
 
       // 1文字づつ約物かどうかを判定
-      const $chars = methods.detectYakumono($elem, settings);
+      var $chars = methods.detectYakumono($elem, settings);
 
       // 約物の前後関係を判定
       methods.detectYakumonoContext($chars, settings);
@@ -74,14 +77,14 @@
     },
 
     // 全角ダッシュを罫線に置換する
-    replaceDash: ($elem, settings) => {
+    replaceDash: function replaceDash($elem, settings) {
       if (settings.replaceDash) {
         $elem.html($elem.html().replace('――', '──'));
       }
     },
 
     // ツメ組みクラスを付与
-    makeTsumegumi: ($elem, settings) => {
+    makeTsumegumi: function makeTsumegumi($elem, settings) {
       if (settings.isTsumegumi !== true) {
         return;
       }
@@ -90,7 +93,7 @@
     },
 
     // 字下げクラスを付与
-    makeJisage: ($elem, settings) => {
+    makeJisage: function makeJisage($elem, settings) {
       if (settings.useJisage !== true) {
         return;
       }
@@ -99,7 +102,7 @@
     },
 
     // 縦組みクラスを付与
-    makeTategumi: ($elem, settings) => {
+    makeTategumi: function makeTategumi($elem, settings) {
       if (settings.isTategumi !== true) {
         return;
       }
@@ -108,7 +111,7 @@
     },
 
     // 行末約物半角クラスを付与
-    makeGyomatsuYakumonoHankaku: ($elem, settings) => {
+    makeGyomatsuYakumonoHankaku: function makeGyomatsuYakumonoHankaku($elem, settings) {
       if (settings.isGyomatsuYakumonoHankaku !== true) {
         return;
       }
@@ -117,7 +120,7 @@
     },
 
     // ベタ組みレイアウトを適用
-    makeBetagumi: ($elem, settings) => {
+    makeBetagumi: function makeBetagumi($elem, settings) {
 
       // ツメ組みがtrueのときは無視する
       if (settings.isBetagumi !== true || settings.isTsumegumi) {
@@ -132,10 +135,10 @@
       if (!settings.isTategumi) {
         $elem.addClass(settings.className.betagumi).width('');
 
-        const fontSizePx = parseInt($elem.css('font-size'), 10);
-        const actualWidthPx = $elem.width();
-        const characters = Math.floor(actualWidthPx / fontSizePx);
-        $elem.width(`${characters}em`);
+        var fontSizePx = parseInt($elem.css('font-size'), 10);
+        var actualWidthPx = $elem.width();
+        var characters = Math.floor(actualWidthPx / fontSizePx);
+        $elem.width(characters + "em");
         return;
       }
 
@@ -143,25 +146,25 @@
       if (settings.isTategumi) {
         $elem.addClass(settings.className.betagumi).height('');
 
-        const fontSizePx = parseInt($elem.css('font-size'), 10);
-        const actualWidthPx = $elem.height();
-        const characters = Math.floor(actualWidthPx / fontSizePx);
-        $elem.height(`${characters}em`);
+        var _fontSizePx = parseInt($elem.css('font-size'), 10);
+        var _actualWidthPx = $elem.height();
+        var _characters = Math.floor(_actualWidthPx / _fontSizePx);
+        $elem.height(_characters + "em");
       }
     },
 
     // blast
-    blast: ($elem, settings) => {
+    blast: function blast($elem, settings) {
       return $elem.blast({
         delimiter: "character",
         tag: 'span',
-        customClass: settings.className.char,
+        customClass: settings.className.char
       });
     },
 
     // 1文字づつ約物かどうかを判定
-    detectYakumono: ($elem, settings) => {
-      const $chars = $elem.find(`.${settings.className.char}`);
+    detectYakumono: function detectYakumono($elem, settings) {
+      var $chars = $elem.find("." + settings.className.char);
 
       $chars.each(function () {
         methods.addYakumonoClass($(this), settings);
@@ -171,20 +174,20 @@
     },
 
     // 文字の種類を判定してクラスを付与する
-    addYakumonoClass: ($char, settings) => {
-      const c = $char.text();
+    addYakumonoClass: function addYakumonoClass($char, settings) {
+      var c = $char.text();
 
-      for (const yakumonoType in settings.dict) {
+      for (var yakumonoType in settings.dict) {
         if (c.match(settings.dict[yakumonoType]) !== null) {
-          $char.addClass(`${settings.className.yakumono} ${settings.className.yakumono}${settings.className.separator}${yakumonoType}`);
+          $char.addClass(settings.className.yakumono + " " + settings.className.yakumono + settings.className.separator + yakumonoType);
           return;
         }
       }
     },
 
     // 約物の前後関係を判定
-    detectYakumonoContext: ($chars, settings) => {
-      let $previous = null;
+    detectYakumonoContext: function detectYakumonoContext($chars, settings) {
+      var $previous = null;
 
       $chars.each(function () {
         $previous = methods.setYakumonoContextClass($previous, $(this), settings);
@@ -192,17 +195,17 @@
     },
 
     // 前後の要素から約物クラスを付与する
-    setYakumonoContextClass: ($previous, $current, settings) => {
-      if ($previous === null || !$previous.hasClass(`${settings.className.yakumono}`)) {
+    setYakumonoContextClass: function setYakumonoContextClass($previous, $current, settings) {
+      if ($previous === null || !$previous.hasClass("" + settings.className.yakumono)) {
         return $current;
       }
 
-      for (const type in settings.dict) {
-        if ($current.hasClass(`${settings.className.yakumono}${settings.className.separator}${type}`)) {
-          $previous.addClass(`${settings.className.yakumono}${settings.className.separator}before${settings.className.separator}${type}`);
+      for (var type in settings.dict) {
+        if ($current.hasClass("" + settings.className.yakumono + settings.className.separator + type)) {
+          $previous.addClass("" + settings.className.yakumono + settings.className.separator + "before" + settings.className.separator + type);
         }
-        if ($previous.hasClass(`${settings.className.yakumono}${settings.className.separator}${type}`)) {
-          $current.addClass(`${settings.className.yakumono}${settings.className.separator}after${settings.className.separator}${type}`);
+        if ($previous.hasClass("" + settings.className.yakumono + settings.className.separator + type)) {
+          $current.addClass("" + settings.className.yakumono + settings.className.separator + "after" + settings.className.separator + type);
         }
       }
 
@@ -210,107 +213,102 @@
     },
 
     // 行頭・行末を判定
-    detectGyotouGyoumatsu: ($chars, settings) => {
-      let previousOffset = null;
-      let currentOffset = null;
-      let $previous;
+    detectGyotouGyoumatsu: function detectGyotouGyoumatsu($chars, settings) {
+      var previousOffset = null;
+      var currentOffset = null;
+      var $previous = void 0;
 
-      const getOffset = ($current) => {
+      var getOffset = function getOffset($current) {
         if (!settings.isTategumi) {
           return $current[0].offsetTop;
         }
         return $current[0].offsetLeft;
       };
 
-      $chars
-        .css('letter-spacing', '')
-        .removeClass(`${settings.className.startOfLine} ${settings.className.endOfLine} ${settings.className.oidashi} ${settings.className.oidashiSmall}`)
-        .each(function () {
-          const $current = $(this);
+      $chars.css('letter-spacing', '').removeClass(settings.className.startOfLine + " " + settings.className.endOfLine + " " + settings.className.oidashi + " " + settings.className.oidashiSmall).each(function () {
+        var $current = $(this);
 
-          currentOffset = getOffset($current);
+        currentOffset = getOffset($current);
 
-          // 初回
-          if (previousOffset === null) {
-            $current.addClass(settings.className.startOfLine);
-            previousOffset = currentOffset;
-            $previous = $current;
-            return;
-          }
-
-          // 前回からY座標が変化した時に改行を検知
-          if (currentOffset > previousOffset) {
-
-            // 1文字手前は行末として判定
-            $previous.addClass(settings.className.endOfLine);
-            const actualOffset = getOffset($current);
-
-            // クラス付与により行頭文字が前の行に追い込まれなかった場合は、現在の文字を行頭として判定
-            if (actualOffset === currentOffset) {
-              $current.addClass(settings.className.startOfLine);
-
-              // この文字が始め括弧だった場合、やはり、その影響で前の行に追い込まれる場合がある
-              // その場合は、1文字前に特別な追い出しクラスを付与する
-              // このクラス(A.)は後述の(B.)とは異なる設定となる
-              // この追い出し量は僅かで良い
-              if (getOffset($current) !== currentOffset) {
-                $previous.addClass(settings.className.oidashiSmall);
-              }
-            }
-
-            // 行頭だった現在の文字が行末に追いこまれてしまった場合
-            if (actualOffset !== currentOffset) {
-
-              // 字間調整機能がオフのとき
-              if (!settings.useLetterSpacingAdjustment || settings.isTategumi) {
-                // このままでは前の文字と現在の文字の間が詰まりすぎてしまうので、
-                // 前の文字に半角分の追い出しクラスを付与する(B.)
-                $previous
-                  .addClass(settings.className.oidashi);
-              }
-
-              // 字間調整機能がオンのときはさらに詳細な計算を開始
-              if (settings.useLetterSpacingAdjustment) {
-                methods.adjustLetterSpacing($previous, settings);
-              }
-            }
-          }
-
+        // 初回
+        if (previousOffset === null) {
+          $current.addClass(settings.className.startOfLine);
           previousOffset = currentOffset;
           $previous = $current;
-        })
-      ;
+          return;
+        }
+
+        // 前回からY座標が変化した時に改行を検知
+        if (currentOffset > previousOffset) {
+
+          // 1文字手前は行末として判定
+          $previous.addClass(settings.className.endOfLine);
+          var actualOffset = getOffset($current);
+
+          // クラス付与により行頭文字が前の行に追い込まれなかった場合は、現在の文字を行頭として判定
+          if (actualOffset === currentOffset) {
+            $current.addClass(settings.className.startOfLine);
+
+            // この文字が始め括弧だった場合、やはり、その影響で前の行に追い込まれる場合がある
+            // その場合は、1文字前に特別な追い出しクラスを付与する
+            // このクラス(A.)は後述の(B.)とは異なる設定となる
+            // この追い出し量は僅かで良い
+            if (getOffset($current) !== currentOffset) {
+              $previous.addClass(settings.className.oidashiSmall);
+            }
+          }
+
+          // 行頭だった現在の文字が行末に追いこまれてしまった場合
+          if (actualOffset !== currentOffset) {
+
+            // 字間調整機能がオフのとき
+            if (!settings.useLetterSpacingAdjustment || settings.isTategumi) {
+              // このままでは前の文字と現在の文字の間が詰まりすぎてしまうので、
+              // 前の文字に半角分の追い出しクラスを付与する(B.)
+              $previous.addClass(settings.className.oidashi);
+            }
+
+            // 字間調整機能がオンのときはさらに詳細な計算を開始
+            if (settings.useLetterSpacingAdjustment) {
+              methods.adjustLetterSpacing($previous, settings);
+            }
+          }
+        }
+
+        previousOffset = currentOffset;
+        $previous = $current;
+      });
     },
 
-    floatFormat: (number, n) => {
-      const _pow = Math.pow(10, n);
+    floatFormat: function floatFormat(number, n) {
+      var _pow = Math.pow(10, n);
       return Math.floor(number * _pow) / _pow;
     },
 
     // 文字間隔を調整して行末のアキをなくす
-    adjustLetterSpacing: ($char, settings) => {
+    adjustLetterSpacing: function adjustLetterSpacing($char, settings) {
 
       // 縦組みモードでは無視
       if (settings.isTategumi) {
         return;
       }
 
-      const chars = methods.getCharsOfLine($char, settings);
-      const lsValue = methods.floatFormat((0.965 / (chars.length)), 5);
+      var chars = methods.getCharsOfLine($char, settings);
+      var lsValue = methods.floatFormat(0.965 / chars.length, 5);
 
-      chars.forEach(($elem) => {
-        $elem.css('letter-spacing', `${lsValue}em`);
+      chars.forEach(function ($elem) {
+        $elem.css('letter-spacing', lsValue + "em");
       });
     },
 
     // 行頭から行末の範囲の文字列を取得する
-    getCharsOfLine: ($char, settings) => {
-      const startOfLine = settings.className.startOfLine;
-      let end = false;
-      const chars = [];
+    getCharsOfLine: function getCharsOfLine($char, settings) {
+      var startOfLine = settings.className.startOfLine;
+      var end = false;
+      var chars = [];
 
-      let $current = $char;
-      let $next;
+      var $current = $char;
+      var $next = void 0;
 
       while (end === false) {
         $next = $current.prev();
@@ -333,10 +331,10 @@
     },
 
     // 再描画する
-    update: ($rootElems, settings) => {
+    update: function update($rootElems, settings) {
       $rootElems.each(function () {
-        const $txt = $(this);
-        const $chars = $txt.find(`.${settings.className.char}`);
+        var $txt = $(this);
+        var $chars = $txt.find("." + settings.className.char);
 
         methods.makeBetagumi($txt, settings);
         methods.detectGyotouGyoumatsu($chars, settings);
@@ -344,23 +342,23 @@
     },
 
     // ウィンドウリサイズを監視する
-    watchResize: ($rootElems, settings) => {
+    watchResize: function watchResize($rootElems, settings) {
       if (!settings.watchResize) return;
-      let timer = null;
-      $(window).on('resize', () => {
-        timer = methods.checkTimer(timer, settings.watchDelay, () => {
+      var timer = null;
+      $(window).on('resize', function () {
+        timer = methods.checkTimer(timer, settings.watchDelay, function () {
           methods.update($rootElems, settings);
         });
       });
     },
 
     // 遅延タイマーをチェック
-    checkTimer: (timer, delay, callback) => {
+    checkTimer: function checkTimer(timer, delay, callback) {
       if (timer !== null) {
         clearTimeout(timer);
       }
 
-      return setTimeout(() => {
+      return setTimeout(function () {
         callback();
       }, delay);
     }
@@ -379,9 +377,9 @@
     // 約物の分類辞書
     dict: {
       hajimekakko: /（|〔|「|『|［|【|〈|《|‘|“/,
-      owarikakko:  /）|〕|」|』|］|】|〉|》|’|”/,
+      owarikakko: /）|〕|」|』|］|】|〉|》|’|”/,
       nakaten: /・|：|；/,
-      kuten:  /。|．/,
+      kuten: /。|．/,
       touten: /、|，/,
       kugiri: /！|？/,
       hyphen: /‐|〜/,
@@ -442,5 +440,4 @@
     // 縦書きモード(実験的)
     isTategumi: false
   };
-
-}));
+});
